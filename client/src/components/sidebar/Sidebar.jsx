7c51ba549811092ea3f67ee354f0aadf1d1c5e97
YohanNayanajith <yohannayanajith40@gmail.com>
2022-08-13 20:38:10 +0530

    second commit

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -17,138 +17,183 @@ import {
 import { Link } from "react-router-dom";
 import PerfectScrollbar from "react-perfect-scrollbar";
 
+import * as React from "react";
+import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import SendIcon from "@mui/icons-material/Send";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import StarBorder from "@mui/icons-material/StarBorder";
+
 export default function Sidebar() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
     <div className="sidebar">
       <PerfectScrollbar>
-        <div className="sidebarWrapper">
-          <div className="sidebarMenu">
-            <h3 className="sidebarTitle">Dashboard</h3>
-            <ul className="sidebarList">
-              <Link to="/" className="link">
-                <li className="sidebarListItem active">
-                  <LineStyle className="sidebarIcon" />
-                  Home
-                </li>
-              </Link>
-              {/* <li className="sidebarListItem">
-              <Timeline className="sidebarIcon" />
-              Analytics
-            </li> */}
-              {/* <li className="sidebarListItem">
-              <TrendingUp className="sidebarIcon" />
-              Sales
-            </li> */}
-              <Link to="/users" className="link">
-                <li className="sidebarListItem">
-                  <PermIdentity className="sidebarIcon" />
-                  Users
-                </li>
-              </Link>
-              <Link to="/products" className="link">
-                <li className="sidebarListItem">
-                  <Storefront className="sidebarIcon" />
-                  Products
-                </li>
-              </Link>
-              <Link to="/payments" className="link">
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+        >
+          <div className="sidebarWrapper">
+            <div className="sidebarMenu">
+              <h3 className="sidebarTitle">Dashboard</h3>
+              <ul className="sidebarList">
+                <Link to="/" className="link">
+                  <li className="sidebarListItem active">
+                    <LineStyle className="sidebarIcon" />
+                    Home
+                  </li>
+                </Link>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <SendIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Sent mail" />
+                </ListItemButton>
+                <Link to="/users" className="link">
+                  <li className="sidebarListItem">
+                    <PermIdentity className="sidebarIcon" />
+                    Users
+                  </li>
+                </Link>
+                <Link to="/products" className="link">
+                  <li className="sidebarListItem">
+                    <Storefront className="sidebarIcon" />
+                    Products
+                  </li>
+                </Link>
+                <Link to="/payments" className="link">
+                  <li className="sidebarListItem">
+                    <AttachMoney className="sidebarIcon" />
+                    Payments
+                  </li>
+                </Link>
+                <Link to="/orders" className="link">
+                  <li className="sidebarListItem">
+                    <BarChart className="sidebarIcon" />
+                    Orders
+                  </li>
+                </Link>
+                <Link to="/reports" className="link">
+                  <li className="sidebarListItem">
+                    <Report className="sidebarIcon" />
+                    Reports
+                  </li>
+                </Link>
+                <Link to="/article" className="link">
+                  <li className="sidebarListItem">
+                    <WorkOutline className="sidebarIcon" />
+                    Articles
+                  </li>
+                </Link>
+                <Link to="/email" className="link">
+                  <li className="sidebarListItem">
+                    <Email className="sidebarIcon" />
+                    Send Email
+                  </li>
+                </Link>
+
+                <ListItemButton>
+                  <ListItemIcon>
+                    <SendIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Sent mail" />
+                </ListItemButton>
+                
+                <ListItemButton onClick={handleClick}>
+                  <ListItemIcon>
+                    <InboxIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Inbox"
+                    sx={{ pl: 0, textAlign: "left" }}
+                  />
+                  {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <ListItemButton sx={{ pl: 4 }}>
+                      <ListItemIcon>
+                        <StarBorder />
+                      </ListItemIcon>
+                      <ListItemText primary="Starred" />
+                    </ListItemButton>
+                  </List>
+                </Collapse>
+              </ul>
+            </div>
+            <div className="sidebarMenu">
+              <h3 className="sidebarTitle">Quick Menu</h3>
+              <ul className="sidebarList">
+                <Link to="/users" className="link">
+                  <li className="sidebarListItem">
+                    <PermIdentity className="sidebarIcon" />
+                    Users
+                  </li>
+                </Link>
+                <Link to="/products" className="link">
+                  <li className="sidebarListItem">
+                    <Storefront className="sidebarIcon" />
+                    Products
+                  </li>
+                </Link>
                 <li className="sidebarListItem">
                   <AttachMoney className="sidebarIcon" />
-                  Payments
+                  Transactions
                 </li>
-              </Link>
-              <Link to="/orders" className="link">
                 <li className="sidebarListItem">
                   <BarChart className="sidebarIcon" />
-                  Orders
+                  Reports
                 </li>
-              </Link>
-              <Link to="/reports" className="link">
+              </ul>
+            </div>
+            <div className="sidebarMenu">
+              <h3 className="sidebarTitle">Notifications</h3>
+              <ul className="sidebarList">
+                <li className="sidebarListItem">
+                  <MailOutline className="sidebarIcon" />
+                  Mail
+                </li>
+                <li className="sidebarListItem">
+                  <DynamicFeed className="sidebarIcon" />
+                  Feedback
+                </li>
+                <li className="sidebarListItem">
+                  <ChatBubbleOutline className="sidebarIcon" />
+                  Messages
+                </li>
+              </ul>
+            </div>
+            <div className="sidebarMenu">
+              <h3 className="sidebarTitle">Staff</h3>
+              <ul className="sidebarList">
+                <li className="sidebarListItem">
+                  <WorkOutline className="sidebarIcon" />
+                  Manage
+                </li>
+                <li className="sidebarListItem">
+                  <Timeline className="sidebarIcon" />
+                  Analytics
+                </li>
                 <li className="sidebarListItem">
                   <Report className="sidebarIcon" />
                   Reports
                 </li>
-              </Link>
-              <Link to="/article" className="link">
-                <li className="sidebarListItem">
-                  <WorkOutline className="sidebarIcon" />
-                  Articles
-                </li>
-              </Link>
-              <Link to="/email" className="link">
-                <li className="sidebarListItem">
-                  <Email className="sidebarIcon" />
-                  Send Email
-                </li>
-              </Link>
-              {/* <Link to="/post" className="link">
-              <li className="sidebarListItem">
-                <WorkOutline className="sidebarIcon" />
-                Posts
-              </li>
-            </Link> */}
-            </ul>
+              </ul>
+            </div>
           </div>
-          <div className="sidebarMenu">
-            <h3 className="sidebarTitle">Quick Menu</h3>
-            <ul className="sidebarList">
-              <Link to="/users" className="link">
-                <li className="sidebarListItem">
-                  <PermIdentity className="sidebarIcon" />
-                  Users
-                </li>
-              </Link>
-              <Link to="/products" className="link">
-                <li className="sidebarListItem">
-                  <Storefront className="sidebarIcon" />
-                  Products
-                </li>
-              </Link>
-              <li className="sidebarListItem">
-                <AttachMoney className="sidebarIcon" />
-                Transactions
-              </li>
-              <li className="sidebarListItem">
-                <BarChart className="sidebarIcon" />
-                Reports
-              </li>
-            </ul>
-          </div>
-          <div className="sidebarMenu">
-            <h3 className="sidebarTitle">Notifications</h3>
-            <ul className="sidebarList">
-              <li className="sidebarListItem">
-                <MailOutline className="sidebarIcon" />
-                Mail
-              </li>
-              <li className="sidebarListItem">
-                <DynamicFeed className="sidebarIcon" />
-                Feedback
-              </li>
-              <li className="sidebarListItem">
-                <ChatBubbleOutline className="sidebarIcon" />
-                Messages
-              </li>
-            </ul>
-          </div>
-          <div className="sidebarMenu">
-            <h3 className="sidebarTitle">Staff</h3>
-            <ul className="sidebarList">
-              <li className="sidebarListItem">
-                <WorkOutline className="sidebarIcon" />
-                Manage
-              </li>
-              <li className="sidebarListItem">
-                <Timeline className="sidebarIcon" />
-                Analytics
-              </li>
-              <li className="sidebarListItem">
-                <Report className="sidebarIcon" />
-                Reports
-              </li>
-            </ul>
-          </div>
-        </div>
+        </List>
       </PerfectScrollbar>
     </div>
   );
