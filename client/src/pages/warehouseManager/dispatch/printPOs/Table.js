b95a132c1fa53cc02163add93f5a18a0629fcf96
Minu98 <minuratnayake@gmail.com>
2022-10-24 22:58:41 +0530

    Invoice changes

--- client/src/pages/warehouseManager/dispatch/printPOs/Table.js
+++ client/src/pages/warehouseManager/dispatch/printPOs/Table.js
@@ -6,20 +6,20 @@ export default function ({list}) {
     <table width="100%" className='mb-10'>
          <thead>
                 <tr className='bg-gray-100 p-1'>
-                <td className='font-bold'>Description</td>
-                <td className='font-bold'>Quantity</td>
-                <td className='font-bold'>Price</td>
-                <td className='font-bold'>Amount</td>
+                <td className='font-bold'style={{align:"canter"}}>Description</td>
+                <td className='font-bold'style={{align:"canter"}}>Quantity</td>
+                {/* <td className='font-bold'>Price</td>
+                <td className='font-bold'>Amount</td> */}
                 </tr>
          </thead>
         {list.map(({id, desc,quantity,price,amount}) =>(
             <React.Fragment key={id}>
-            <tbody>
+            <tbody style={{alignItems:"canter"}}>
                 <tr>
-                <td>{desc}</td>
-                <td>{quantity}</td>
-                <td>{price}</td>
-                <td>{amount}</td>
+                <td style={{alignSelf:"canter"}}>{desc}</td>
+                <td style={{align:"canter"}}>{quantity}</td>
+                {/* <td>{price}</td>
+                <td>{amount}</td> */}
                 </tr>
             </tbody>
             </React.Fragment>
